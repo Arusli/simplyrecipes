3634a4c6ed9fb840ea833ee88defd5bee9d8d42f
Andrew Rusli <andreworusli@gmail.com>
2021-09-18 13:28:03 -0400

    Static image set up, 35

--- src/examples/Images.js
+++ src/examples/Images.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import styled from 'styled-components';
+import { StaticImage } from 'gatsby-plugin-image';
+
 
 const Images = () => {
     return (
         <Wrapper>
-        <h2>gatsby image</h2>   
+            <StaticImage src='../assets/images/big.jpg' alt='food' />
+            <StaticImage src='https://res.cloudinary.com/diqqf3eq2/image/upload/v1613583026/course%20slides/js-2_mo9uqy.png' alt='code' />
+            <h2>gatsby image</h2>   
         </Wrapper>
     )
 }
 
 const Wrapper = styled.section`
-
+img {
+    width: 200px;
+}
 `
 
 export default Images
