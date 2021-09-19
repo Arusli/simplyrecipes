776e51f9e0d604672096ee29b5b86232394749b6
Andrew Rusli <andreworusli@gmail.com>
2021-09-19 09:42:50 -0400

    options example 37

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <footer class='page-footer'>
+        <footer className='page-footer'>
             <p>&copy; {new Date().getFullYear()}<span>Simple Recipes</span>. Built with{" "} <a href='https://www.gatbsyjs.com/'>Gatsby</a> </p>
         </footer>
     )
