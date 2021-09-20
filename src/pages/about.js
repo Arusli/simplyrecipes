7cb7c33bd095b1767ccc5c0ec5352fc933d66fbc
Andrew Rusli <andreworusli@gmail.com>
2021-09-20 09:42:21 -0400

    about page, 40

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,15 +1,31 @@
 import React from 'react';
 import {Link} from 'gatsby';
 import Layout from '../components/Layout';
+import {StaticImage} from 'gatsby-plugin-image';
 
-const about = () => {
+const About = () => {
     return (
         <Layout>    
-                <div>
-                    <h1>About Page</h1>
-                </div>
+            <main className='page'>
+                <section className='about-page'>
+                    <article>
+                        <h2>I'm baby coloring turkey.</h2>
+                        <p>I'm baby chambray authentic gochujang, intelligentsia truffaut ugh fanny pack mixtape. Yr butcher fingerstache small batch raw denim twee, flannel hoodie affogato edison bulb hammock deep v polaroid.</p>
+                        <p>Actually kogi lo-fi lumbersexual, cornhole craft beer put a bird on it blog raclette. Pop-up cold-pressed gentrify hell of mixtape everyday carry.</p>
+                        <Link to='/contact' className='btn'>
+                            contact
+                        </Link>
+                    </article>
+                    <StaticImage 
+                    src='../assets/images/about.jpeg' 
+                    alt='person pouring salt in bowl'
+                    className='about-img'
+                    placeholder='blurred'
+                    />
+                </section>
+            </main>
         </Layout>
     )
 }
 
-export default about
+export default About
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,10 @@
 import React from "react";
-import {Link} from 'gatsby';
 import Layout from '../components/Layout.js';
-import Images from '../examples/Images';
 
 export default function Home() {
   return (
     <Layout>
         <h1>Home Page</h1>
-        <Images />
     </Layout>
   )
 }
