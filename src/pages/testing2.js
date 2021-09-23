8a901e8f03cf3867a6e8892cbaef9031450a7b8c
Andrew Rusli <andreworusli@gmail.com>
2021-09-23 10:34:26 -0400

    61 gatsby image data

---
+++ src/pages/testing2.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {graphql} from 'gatsby'
+import Layout from '../components/Layout'
+import Gallery from '../examples/Gallery'
+
+const Testing2 = () => {
+    return (
+        <Layout>
+            <main className='page'>
+                <Gallery />
+            </main>
+        </Layout>
+    )
+}
+
+export default Testing2
