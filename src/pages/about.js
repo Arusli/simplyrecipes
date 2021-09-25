20634461a025722721ba4f8a44da467a6013e312
Andrew Rusli <andreworusli@gmail.com>
2021-09-25 11:55:41 -0400

    76, Featured Recipes, essential

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import {Link} from 'gatsby';
 import Layout from '../components/Layout';
+import {Link, graphql} from 'gatsby';
 import {StaticImage} from 'gatsby-plugin-image';
+import RecipesList from '../components/RecipesList';
 
-const About = () => {
+const About = ({data:{allContentfulRecipe:{nodes:recipes}}}) => {
     return (
         <Layout>    
             <main className='page'>
                 <section className='about-page'>
                     <article>
                         <h2>I'm baby coloring turkey.</h2>
-                        <p>I'm baby chambray authentic gochujang, intelligentsia truffaut ugh fanny pack mixtape. Yr butcher fingerstache small batch raw denim twee, flannel hoodie affogato edison bulb hammock deep v polaroid.</p>
+                        <p>I'm baby chambray authentic gochujang. Yr butcher fingerstache small batch raw denim twee, flannel hoodie affogato edison bulb hammock deep v polaroid.</p>
                         <p>Actually kogi lo-fi lumbersexual, cornhole craft beer put a bird on it blog raclette. Pop-up cold-pressed gentrify hell of mixtape everyday carry.</p>
                         <Link to='/contact' className='btn'>
                             contact
@@ -23,9 +24,31 @@ const About = () => {
                     placeholder='blurred'
                     />
                 </section>
+                <section className='featured-recipes'>
+                    <h5>Look at this Awesomesauce</h5>
+                    <RecipesList recipes={recipes} />
+                </section>
             </main>
         </Layout>
     )
 }
+
+export const query = graphql`
+  {
+    allContentfulRecipe(
+      sort: {fields: title, order: ASC}
+      filter: {featured: {eq: true}}
+    ) {
+      nodes {
+        id
+        cookTime
+        prepTime
+        image {
+          gatsbyImageData(layout: CONSTRAINED, placeholder: BLURRED)
+        }
+      }
+    }
+  }
+`
 
 export default About
